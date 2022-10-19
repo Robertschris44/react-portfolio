5a1519b825ea66d5db903d68857c7bde2046e0b8
Chris Key <robertschris010@gmail.com>
2022-10-19 17:54:46 -0500

    working on react

---
+++ src/components/Contact/index.js
@@ -0,0 +1,19 @@
+import react from 'react'
+// import photo from '' pass in photo
+
+
+const Contact = () => {
+    return(
+        <section className='contact-container'>
+            <div>
+            {/* <img src={photo} */}
+            </div>
+            <h2 className='section-title'>Contact Me</h2>
+
+
+        </section>
+    );
+};
+
+
+export default Contact;

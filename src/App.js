9b07c92be6b16b95ff81c7de20d6a9e6005521a4
Chris Key <robertschris010@gmail.com>
2022-10-22 13:15:23 -0500

    working on about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React,{useState} from 'react';
 import Contact from './components/Contact';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -8,7 +8,34 @@ import Project from './components/Project';
 
 
 function App() {
+
+  const [categories] = useState([
+    {
+      name: "About me",
+      description:
+      "Who am I?",
+    },
+    {
+      name: "Contact",
+      description:
+      "Reach out:",
+    },
+    {
+      name: "Portfolio",
+      description: 
+      "Hello",
+    },
+    {
+      name: "Resume",
+      description: "Download Resume"
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
+    //add nav,footer,gallery
     <div className="App">
       <Contact />
     </div>
---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+    return(
+        <section>
+            <h1 id="about">About Me</h1>
+            <p>I am a full stack web developer. My experience: Javascript, HTML, Bootstrap, CSS, MongoDB, Express.js, React.js, Node.js, MySQL, Sequelize and GraphQL. </p>
+            <br />
+            <p>After wanting to be in the tech field for many years, I have decided to take the leap of faith and make a career change to be a web developer. I have a bachelors in Business Administration from Sam Houston University. I am currently a claims adjuster, licensed in over 5 states! I took this full stack development course because I want a new challenging career! </p>
+            <br />
+            <h2>Core Values</h2>
+            <ul>
+                <li>Be mindfully present with others and engage fully in whatever I am doing</li>
+                <li>To be genuine and always to to myself</li>
+                <li>It is important that I keep challenging myself to learn, grow, and improve</li>
+                <li>Change is good and welcomed</li>
+                <li>Simplicity should be used whenever possible</li>
+            </ul>
+        </section>
+    )
+}
+
+export default About;

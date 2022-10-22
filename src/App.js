b6b49a0c68332cf63cc83acc29eb86ed0871576a
Chris Key <robertschris010@gmail.com>
2022-10-22 12:06:39 -0500

    importing components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import Contact from './components/Contact';
+import About from './components/About';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+import Gallery from './components/Gallery';
+import Project from './components/Project';
+
 
 function App() {
   return (

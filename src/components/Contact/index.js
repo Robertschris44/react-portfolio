83042d8dcb346a5bca0264b890ee8f79a8bc5c3f
Chris Key <robertschris010@gmail.com>
2022-10-24 23:19:54 -0500

    contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,18 +5,74 @@ import {validateEmail, capitalizeFirstLetter} from '../.././utils/helpers';
 
 function Contact(){
 
-    const[fromState, setFormState] = useState({name: '',email:'',message:''});
+    const[formState, setFormState] = useState({name: '',email:'',message:''});
+    const {name, email, message} = formState;
+    const[errorMessage, setErrorMessage] =useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        if(!errorMessage){
+            setFormState({[event.target.name]:event.target.value})
+            console.log('Form', formState);
+        }
+    };
+    
+
+    function handleChange(event){
+        if(event.target.name === 'email'){
+            const isValid = validateEmail(event.target.value);
+            console.log(isValid);
+            if(!isValid){
+                setErrorMessage('Not a valid email');
+            }
+            else{
+                setErrorMessage('');
+            }
+            }
+            else {
+                if(!event.target.value.length){
+                    setErrorMessage(capitalizeFirstLetter((`${event.target.name} is required`)));
+                }
+                else {
+                    setErrorMessage('');
+                }
+            }
+            if(!errorMessage){setFormState({...formState,[event.target.name]:(event.target.value).trim()})}
+        }
+    }
     return(
         <section className='contact-container'>
-            <div>
-            {/* <img src={photo} */}
-            </div>
-            <h2 className='section-title'>Contact Me</h2>
+            <h1 className='section-title'>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='name'>Your Name:</label>
+                    <input type="text" name="name" onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor='email'>Your email address:</label>
+                    <input type="email" name='email' onBlur={handleChange}></input>
+                </div>
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' rows="4" onBlur={handleChange}></textarea>
+                </div>
 
+                <div className=''> 
+                    <button className='contactBtn' type='submit'>Submit</button>
+                </div>
+                
+                {errorMessage && (
+                <div>
+                    <p className='error-text'>{errorMessage}</p>
+                </div>)}
+               
+
+            </form>
+            
 
         </section>
     );
-};
+
 
 
 export default Contact;

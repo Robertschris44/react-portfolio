c21e82a2d8c04392b02ba1919558160caaa4e74e
Chris Key <robertschris010@gmail.com>
2022-10-26 22:56:37 -0500

    updating

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Project({title,href,gitHub}) {
+function Project({title,href,image,gitHub}) {
     return (
         <section className="project-section">
             <div>
                 <h3>{title}</h3>
                 <a href={gitHub} target="_blank">
 
+                </a>
+
+                <a href={href} target="_blank">
+                    <img className="img-main" src={image} />
                 </a>
             </div>
 

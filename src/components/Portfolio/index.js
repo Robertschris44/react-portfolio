dae559d6738ecca7540057e7d2f1966508f34cb7
Chris Key <robertschris010@gmail.com>
2022-10-26 22:09:27 -0500

    bcs

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,11 @@
 import React, {useState} from 'react';
+import Project from '../Project';
+import imgAbsence from '../../assets/Absence snippet.jpg'
+import imgEmpoloyeetracker from '../../assets/tracker snippet'
+import imgNewyorktimes from '../../assets/newyork snippet'
+import imgNotetaker from '../../assets/Note taker snippet'
+import imgRunbuddy from '../../assets/Runbuddy snippet'
+import imgScheduler from '../../assets/Scheduler screenshot'
 
 
  function Portfolio() {
@@ -6,40 +13,71 @@ import React, {useState} from 'react';
         {
             title: 'Employee Tracker',
             href: 'https://robertschris44.github.io/employee-tracker/',
-            image: 'placehere',
+            image: `${imgEmpoloyeetracker}`,
             github: 'https://github.com/Robertschris44/employee-tracker'
         },
         {
             title: 'Run Buddy',
             href: 'https://robertschris44.github.io/run-buddy/',
-            image: 'placehere',
+            image: `${imgRunbuddy}`,
             github: 'https://github.com/Robertschris44/run-buddy'
         },
         {
             title: 'Absence Reporting',
             href: 'https://absence-reporting.herokuapp.com/',
-            image: 'placehere',
+            image: `${imgAbsence}`,
             github: 'https://github.com/Robertschris44/absence-reporting'
         },
         {
             title: 'New York times',
             href: 'https://robertschris44.github.io/animal-app/',
-            image: 'placehere',
+            image: `${imgNewyorktimes}`,
             github: 'https://github.com/Robertschris44/animal-app'
         },
         {
-            title: 'ReadMe Generator',
-            href: 'https://robertschris44.github.io/readme-generator/',
-            image: 'placehere',
-            github: 'https://github.com/Robertschris44/readme-generator'
+            title: 'Work Scheduler',
+            href: 'https://robertschris44.github.io/workday-scheduler/',
+            image: `${imgScheduler}`,
+            github: 'https://github.com/Robertschris44/workday-scheduler'
         },
         {
             title: 'Note Taker',
             href: 'https://lit-wave-76729.herokuapp.com/',
-            image: 'placehere',
+            image: `${imgNotetaker}`,
             github: 'https://github.com/Robertschris44/note-taker'
         },
     ])
+
+
+    return (
+        <div>
+
+            <section id='projects'>
+                <h1 className='title-project text-center py-3'>Projects</h1>
+
+                <div className='justify-content-between'>
+                    
+                    {projects.map((project,i) => (
+                        <Project
+                        title={project.title}
+                        href={project.href}
+                        imgSrc={project.imgSrc}
+                        gitHub={project.gitHub}
+                        />
+                    ))}
+
+                </div>
+
+
+
+            </section>
+
+
+
+
+
+        </div>
+    )
  }
 
 export default Portfolio;
